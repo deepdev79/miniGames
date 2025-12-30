60b8ad665796c6db00e958b6b053cfae94324e26
Deepankar <deepankar7493@gmail.com>
2025-12-30 10:40:55 +0530

    simonsays button can auto flash

--- src/pages/SimonSays.tsx
+++ src/pages/SimonSays.tsx
@@ -6,39 +6,78 @@ const initialState = [
   {
     id: 1,
     bgColor: "#f3f711ff",
+    orgBgColor: "#f3f711ff",
+    popColor: "#868807ff",
   },
   {
     id: 2,
     bgColor: "#e60d0dff",
+    orgBgColor: "#e60d0dff",
+    popColor: "#690404ff",
   },
   {
     id: 3,
     bgColor: "#0606f8ff",
+    orgBgColor: "#0606f8ff",
+    popColor: "#040479ff",
   },
   {
     id: 4,
     bgColor: "#06bd31ff",
+    orgBgColor: "#06bd31ff",
+    popColor: "#025214ff",
   },
 ];
 
+function reducer(state, action) {
+  if (action.type === "flash") {
+    return state.map((btn) => ({
+      ...btn,
+      bgColor: btn.id === action.payload ? btn.popColor : btn.bgColor,
+    }));
+  } else if (action.type === "unFlash") {
+    return state.map((btn) => ({
+      ...btn,
+      bgColor: btn.id === action.payload ? btn.orgBgColor : btn.bgColor,
+    }));
+  }
+
+  return state;
+}
+
 function SimonSays() {
-  const [{ changeColor }, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function memoryFlash() {
+    const rng = Math.floor(Math.random() * 4) + 1;
+    dispatch({ type: "flash", payload: rng });
+
+    setTimeout(function () {
+      dispatch({ type: "unFlash", payload: rng });
+    }, 500);
+  }
+
   return (
     <div>
       <div className={styles.gamebox}>
-        {initialState.map((btn) => (
-          <Button bgColor={btn.bgColor} key={btn.id}>
-            Test
-          </Button>
+        {state.map((btn) => (
+          <Button
+            onClick={() => dispatch({ type: "click", payload: btn.id })}
+            bgColor={btn.bgColor}
+            key={btn.id}
+          ></Button>
         ))}
       </div>
+      <Button bgColor={"#ffff"} onClick={() => memoryFlash()}></Button>
     </div>
   );
 }
 
-function Button({ bgColor }) {
+function Button({ bgColor, onClick }) {
   return (
-    <motion.button style={{ backgroundColor: bgColor }}>test</motion.button>
+    <motion.button style={{ backgroundColor: bgColor }} onClick={onClick}>
+      test
+    </motion.button>
   );
 }
 export default SimonSays;
